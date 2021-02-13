d30e3048ca630f1f8de297323855673c9821df2c
ZiaTahir504 <mariatahir504@gmail.com>
2021-02-13 12:50:49 -0600

    text to speech works, no animation

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import './App.css';
 
 const App = () => {
 
+  if ('speechSynthesis' in window) {
+    // Speech Synthesis supported
+    alert("Happy Valentines Day! Click on parts of my face to have me recite poetry.")
+   } else {
+     // Speech Synthesis Not Supported
+     alert("Sorry, your browser doesn't support text to speech!");
+   }
+
+  const speech = (text, n) => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text[n].lines.toString().replace(/[^\w.']+/g, " ");
+    msg.rate = 1;
+    msg.pitch = 1;
+    window.speechSynthesis.speak(msg);
+  }
+
   const earClick = () => {
     // 17 - 22 are 14 lines long <3
     let n = Math.floor(Math.random() * (22 - 17 + 1)) + 17;
     fetch(`https://poetrydb.org/title/hear/lines.json`, { mode: 'cors' })
       .then((res) => res.json())
-      .then((data) => {
-        let msg = new SpeechSynthesisUtterance();
-        msg.text = data[n].lines.toString().replace(/[^\w.']+/g, " ");
-        window.speechSynthesis.speak(msg);
-      })
+      .then((data) => speech(data, n))
       .catch((err) => console.error(err.message));
   };
 
@@ -21,11 +33,7 @@ const App = () => {
     let n = Math.floor(Math.random() * (15 - 10 + 1) + 10)
     fetch(`https://poetrydb.org/title/see/lines.json`, { mode: 'cors' })
       .then((res) => res.json())
-      .then((data) => {
-        let msg = new SpeechSynthesisUtterance();
-        msg.text = data[n].lines.toString().replace(/[^\w.']+/g, " ");
-        window.speechSynthesis.speak(msg);
-      })
+      .then((data) => speech(data, n))
       .catch((err) => console.error(err.message));
   };
   
@@ -33,16 +41,13 @@ const App = () => {
     // only 0 is 14 lines long :( </3
     fetch(`https://poetrydb.org/title/speak/lines.json`, { mode: 'cors' })
       .then((res) => res.json())
-      .then((data) => {
-        let msg = new SpeechSynthesisUtterance();
-        msg.text = data[0].lines.toString().replace(/[^\w.']+/g, " ");
-        window.speechSynthesis.speak(msg);
-      })
+      .then((data) => speech(data, 0))
       .catch((err) => console.error(err.message));
   };
 
   return (
     <Fragment>
+        <div className="heart"></div>
         <div className="head">
           <div className="top-fur"></div>
           <div className="ears">
